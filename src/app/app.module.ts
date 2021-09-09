d14b9ffe71b3be905c76072e97711b74b37ea4f0
anu prabha <anuprabhamds18@gmail.com>
2021-09-09 15:11:53 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MyLibDemoModule } from 'my-lib-demo';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MyLibDemoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
